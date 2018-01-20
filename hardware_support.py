8e66d6f0ba2a20fae60b2ab46c714e17acf551a9
Janis <janis@jaggedverge.com>
2018-01-20 19:27:56 +1100

    Start implementing hardware support classes

--- hardware_support.py
+++ hardware_support.py
@@ -6,11 +6,19 @@ class Architecture:
         :runtime_check: a function to test at runtime if the code is running
                         on this architecture.
         """
-        raise NotImplementedError
+        self.name = name
+        self.runtime_check = runtime_check
+
+    def __repr__(self):
+        return "Architecture({},{})".format(
+            self.name,
+            self.runtime_check
+        )
+
 
 class InstructionSet:
     """The instruction set being supported"""
-    def __init__(self, name, runtime_check):
+    def __init__(self, architecture, name, runtime_check):
         """
         :name: The name of the instruction set that needs to be supported
                examples Intel: SSE, AVX
@@ -18,4 +26,13 @@ class InstructionSet:
         :runtime_check: A function to test at runtime if this instruction
                         set is supported
         """
-        raise NotImplementedError
+        self.architecture = architecture
+        self.name = name
+        self.runtime_check = runtime_check
+
+    def __repr__(self):
+        return "InstructionSet({},{},{})".format(
+            self.architecture,
+            self.name,
+            self.runtime_check
+        )
